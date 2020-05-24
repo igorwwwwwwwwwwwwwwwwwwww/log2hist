d74ad81756c65090adeb36ae664755e043639236
igor <igor@igor.io>
2020-05-25 01:26:26 +0200

    refactor benches

--- main_test.go
+++ main_test.go
@@ -20,9 +20,9 @@ func BenchmarkRun1(b *testing.B) {
 	}
 }
 
-func BenchmarkRun10(b *testing.B) {
+func benchmarkRunN(b *testing.B, n int) {
 	buf := bytes.NewBuffer([]byte{})
-	for i := 0; i < 10; i++ {
+	for i := 0; i < n; i++ {
 		buf.Write([]byte(fmt.Sprintf("%d\n", rand.Intn(1000000))))
 	}
 
@@ -32,32 +32,32 @@ func BenchmarkRun10(b *testing.B) {
 			b.Error(err)
 		}
 	}
+}
+
+func BenchmarkRun10(b *testing.B) {
+	benchmarkRunN(b, 10)
 }
 
 func BenchmarkRun100(b *testing.B) {
-	buf := bytes.NewBuffer([]byte{})
-	for i := 0; i < 100; i++ {
-		buf.Write([]byte(fmt.Sprintf("%d\n", rand.Intn(1000000))))
-	}
-
-	for n := 0; n < b.N; n++ {
-		err := run(buf, ioutil.Discard)
-		if err != nil {
-			b.Error(err)
-		}
-	}
+	benchmarkRunN(b, 100)
 }
 
-func BenchmarkRun1000(b *testing.B) {
-	buf := bytes.NewBuffer([]byte{})
-	for i := 0; i < 100; i++ {
-		buf.Write([]byte(fmt.Sprintf("%d\n", rand.Intn(1000000))))
-	}
+func BenchmarkRun1k(b *testing.B) {
+	benchmarkRunN(b, 1000)
+}
 
-	for n := 0; n < b.N; n++ {
-		err := run(buf, ioutil.Discard)
-		if err != nil {
-			b.Error(err)
-		}
-	}
+func BenchmarkRun10k(b *testing.B) {
+	benchmarkRunN(b, 10000)
+}
+
+func BenchmarkRun100k(b *testing.B) {
+	benchmarkRunN(b, 100000)
+}
+
+func BenchmarkRun1m(b *testing.B) {
+	benchmarkRunN(b, 1000000)
+}
+
+func BenchmarkRun2m(b *testing.B) {
+	benchmarkRunN(b, 2000000)
 }
